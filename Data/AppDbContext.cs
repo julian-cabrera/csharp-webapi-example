49b2888f04a178ae04cce5926b3eeeecb3bd1500
Julián Cabrera <julian.e.cabrera@hotmail.com>
2022-06-16 20:19:14 -0300

    setup

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using csharp_webapi_example.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp_webapi_example.Data
+{
+  public class AppDbContext : DbContext
+  {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Book> Books { get; set; }
+  }
+}
